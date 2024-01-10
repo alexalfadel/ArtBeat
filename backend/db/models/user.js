d420b6ba189414fccd579c91c7979a5bdfb4e8d4
Alex Alfadel <alexalfadel@gmail.com>
2024-01-10 15:44:33 -0800

    User table migration and seeds

---
+++ backend/db/models/user.js
@@ -0,0 +1,73 @@
+'use strict';
+const { Model, Validator } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [4, 30],
+        isNotEmail(value) {
+          if (Validator.isEmail(value)) {
+            throw new Error("Cannot be an email.");
+          }
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+        unique: true,
+        validate: {
+          len: [3, 256],
+          isEmail: true
+        }
+    },
+    hashedPassword: {
+      type: DataTypes.STRING,
+      allowNull: false,
+        validate: {
+          len: [60, 60]
+        }
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [0, 256]
+      }
+    },
+    bio: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [0, 256]
+      }
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    profilePic: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [0, 300]
+      }
+    },
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
