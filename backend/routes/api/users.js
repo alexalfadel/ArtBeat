9b533c83782c30a46a2a00d6509f7da05c3e2550
Alex Alfadel <alexalfadel@gmail.com>
2024-01-10 16:30:01 -0800

    added user input validations on user signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -3,12 +3,42 @@ const bcrypt = require('bcryptjs');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
+
+const validateSignup = [
+    check('email')
+      .exists({ checkFalsy: true })
+      .isEmail()
+      .withMessage('Please provide a valid email.'),
+    check('username')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 4 })
+      .withMessage('Please provide a username with at least 4 characters.'),
+    check('username')
+      .not()
+      .isEmail()
+      .withMessage('Username cannot be an email.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 6 })
+      .withMessage('Password must be 6 characters or more.'),
+    check('name')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 3 })
+      .withMessage('Name must be 3 characters or more.'),
+    check('location')
+      .exists({ checkFalsy: true })
+      .withMessage('Location is required'),
+    handleValidationErrors
+  ];
 
 // Sign up
 router.post(
     '/',
+    validateSignup,
     async (req, res) => {
       const { email, password, username, name, bio, location, profilePic } = req.body;
       const hashedPassword = bcrypt.hashSync(password);
